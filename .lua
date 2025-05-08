2659b32bfaf64516115c938eef08167b84c4ba67
TryharderBroV10 <hen156094@gmail.com>
2025-05-08 20:12:27 +0300

    Update .lua

--- .lua
+++ .lua
@@ -4,7 +4,7 @@ if _G.MainScriptLoaded then
  end
  _G.MainScriptLoaded = true
 
- local whitelist = { 7847999917, 
+ local whitelist = { 4649811121, 
  } 
 
  local player = game.Players.LocalPlayer
